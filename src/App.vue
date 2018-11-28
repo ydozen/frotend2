e4674be0196943fccd29c6c82f06d8da4daea9ee
ydozen <ydozen@DozennoMacBook-ea.local>
2018-11-28 11:18:45 +0900

    Vue.js初回

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app id = "app">
+    <div>
+      <v-btn>マテリアルボタン</v-btn>
+      <img src="./assets/logo.png">
+      <router-view/>
+    </div>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
