7b0416ffda17c93147f32bee19e267adf574023d
ydozen <ydozen@DozennoMacBook-ea.local>
2018-11-28 14:20:28 +0900

    axios動作確認

--- src/components/Poll/Index.vue
+++ src/components/Poll/Index.vue
@@ -1,11 +1,43 @@
 <template>
     <div>
-        polls
+    <v-card>
+      <v-container grid-list-lg>
+        <v-layout row wrap>
+          <v-flex xs6 v-for="data in questions" :key="data.id">
+            <v-card color="primary" class="white--text">
+              <v-card-title primary-title>
+                <div>{{ data.questionText }}</div>
+              </v-card-title>
+              <v-card-text>
+                <div>{{ data.pubDate }}</div>
+              </v-card-text>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-card>
     </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'PollIndex',
+
+  data () {
+    return {
+      questions: [],
+    }
+  },
+  methods: {
+    fetchData () {
+      axios.get('http://localhost:8000/api/1.0/questions/').then(res => {
+        this.questions = res.data.results
+      })
+    },
+  },
+  mounted () {
+    this.fetchData()
+  },
 }
 </script>
